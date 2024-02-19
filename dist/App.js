8f0180c5498fcf4b10b634f19206183791787101
manzitresor <83731517+manzitresor@users.noreply.github.com>
2024-02-20 00:28:15 +0200

    fetching user input from form

--- dist/App.js
+++ dist/App.js
@@ -1,15 +1,69 @@
 "use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+function autobind(_, _2, descriptor) {
+    const originMethod = descriptor.value;
+    const adjdescriptor = {
+        configurable: true,
+        get() {
+            const boundFn = originMethod.bind(this);
+            return boundFn;
+        }
+    };
+    return adjdescriptor;
+}
 class ProjectInput {
     constructor() {
         this.templateElement = document.getElementById('project-input');
         this.hostElement = document.getElementById('app');
         const importedNode = document.importNode(this.templateElement.content, true);
         this.element = importedNode.firstElementChild;
+        this.element.id = 'user-input';
+        this.titleInputelement = this.element.querySelector('#title');
+        this.descriptionInputelement = this.element.querySelector('#description');
+        this.peopleInputelement = this.element.querySelector('#people');
+        this.configure();
         this.attach();
+    }
+    gatherUserInput() {
+        const enteredTitle = this.titleInputelement.value;
+        const enteredDescription = this.descriptionInputelement.value;
+        const enteredPeople = this.peopleInputelement.value;
+        if (enteredTitle.trim().length === 0 || enteredDescription.trim().length === 0 || enteredPeople.trim().length === 0) {
+            alert('Please enter valid information');
+            return;
+        }
+        else {
+            return [enteredTitle, enteredDescription, +enteredPeople];
+        }
+    }
+    clearInput() {
+        this.titleInputelement.value = '';
+        this.descriptionInputelement.value = '';
+        this.peopleInputelement.value = '';
+    }
+    handlesubmit(event) {
+        event.preventDefault();
+        const userInput = this.gatherUserInput();
+        if (Array.isArray(userInput)) {
+            const [title, desc, peop] = userInput;
+            console.log(title, desc, peop);
+            this.clearInput();
+        }
+    }
+    configure() {
+        this.element.addEventListener('submit', this.handlesubmit.bind(this));
     }
     attach() {
         this.hostElement.insertAdjacentElement('afterbegin', this.element);
     }
 }
+__decorate([
+    autobind
+], ProjectInput.prototype, "handlesubmit", null);
 const prjInput = new ProjectInput();
 //# sourceMappingURL=App.js.map
--- src/App.ts
+++ src/App.ts
@@ -1,14 +1,74 @@
+// autobinded
+function autobind(_: any,_2: string, descriptor: PropertyDescriptor) {
+    const originMethod = descriptor.value
+    const adjdescriptor: PropertyDescriptor = {
+        configurable: true,
+        get() {
+            const boundFn = originMethod.bind(this)
+            return boundFn
+        }
+    }
+
+    return adjdescriptor
+}
+
+
 class ProjectInput {
     templateElement: HTMLTemplateElement
     hostElement: HTMLDivElement
     element: HTMLFormElement
+    titleInputelement: HTMLInputElement
+    descriptionInputelement: HTMLInputElement
+    peopleInputelement: HTMLInputElement
 
     constructor() {
         this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement
         this.hostElement =  document.getElementById('app')! as HTMLDivElement
         const importedNode = document.importNode(this.templateElement.content,true)
         this.element = importedNode.firstElementChild as HTMLFormElement
-        this.attach()
+        this.element.id = 'user-input';
+
+        this.titleInputelement = this.element.querySelector('#title')! as HTMLInputElement;
+        this.descriptionInputelement = this.element.querySelector('#description') as HTMLInputElement;
+        this.peopleInputelement =  this.element.querySelector('#people') as HTMLInputElement;
+
+        this.configure();
+        this.attach();
+    }
+
+ private gatherUserInput(): [string,string,number] | void{
+    const enteredTitle = this.titleInputelement.value;
+    const enteredDescription = this.descriptionInputelement.value;
+    const enteredPeople = this.peopleInputelement.value;
+    if(enteredTitle.trim().length === 0 || enteredDescription.trim().length === 0 || enteredPeople.trim().length === 0) {
+        alert('Please enter valid information')
+        return;
+    } else {
+        return [enteredTitle,enteredDescription, +enteredPeople];
+    }
+
+ }
+
+ private clearInput() {
+    this.titleInputelement.value = '';
+    this.descriptionInputelement.value = '';
+    this.peopleInputelement.value = ''
+
+ }
+
+   @autobind
+    private handlesubmit(event: Event) {
+        event.preventDefault();
+        const userInput = this.gatherUserInput();
+        if(Array.isArray(userInput)) {
+            const [title,desc,peop] = userInput;
+            console.log(title,desc,peop);
+            this.clearInput();
+        }
+    }
+
+    private configure() {
+        this.element.addEventListener('submit',this.handlesubmit.bind(this))
     }
 
     private attach() {
